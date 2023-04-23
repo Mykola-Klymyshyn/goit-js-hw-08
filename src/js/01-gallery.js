7003bcc4559d0247c05d50abf18935231e1026d4
Mykola-Klymyshyn <klymyshyn33@gmail.com>
2023-04-23 20:54:16 +0300

    add refactoring

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,57 +1,24 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 const galleryEl = document.querySelector('.gallery');
-const galleryMarkup = galleryItems.map(({preview, original, description}) => 
-    `<div class="gallery__item">
-        <a class="gallery__link" href="${original}">
-            <img
-                class="gallery__image"
-                src="${preview}"
-                data-source="${original}"
-                alt="${description}"
-            />
-        </a>
-    </div>`
-).join('');
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`
+  )
+  .join('');
 
 galleryEl.innerHTML = galleryMarkup;
-galleryEl.addEventListener('click', onImageClick);
-let instance;
-function onImageClick(event) {
-    event.preventDefault();
-    if (event.target.nodeName !== 'IMG') {
-        return;
-    }
-    instance = basicLightbox.create(
-        `
-        <img src="${event.target.dataset.source}" width="800" height="600">`, {
-        onShow: () => {
-            escapeListener();
-        },
-        onClose: () => {
-            removeEscClose();
-        }
-    });
 
-    instance.show();    
-};
-function escapeListener() {
-    window.addEventListener('keydown', closeWithEsc)
-}
-
-
-function closeWithEsc() {
-        /* console.log(event.code); */ // Event listener check
-        if (event.code === 'Escape') {
-            instance.close();
-    };
-}
-
-
-function removeEscClose() {
-    window.removeEventListener('keydown', closeWithEsc);
-}
-console.log(galleryItems);
+const lightBox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  scrollZoom: false,
+});
